ea96624736c72903122aebd523f43b9a816228d1
mnguye49 <nosquishy101@gmail.com>
2023-04-25 16:13:40 -0400

    Update startingData.sql

--- database/startingData.sql
+++ database/startingData.sql
@@ -10,20 +10,20 @@ VALUES ('llamport', '9171d14954eeda4e70777c23d98e349818125cdaeb884ff97ebf8cc0a9c
 INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_name`)
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
-INSERT into `inventory` ( `item_name`,`flavor`, `frosting`,`filling_one`, `filling_two`, `toppings`, `size`)
+INSERT into `inventory` ( `item_name`,`flavor`, `frosting`,`filling_one`, `filling_two`, `toppings`, `size`,`image_url`)
 VALUES ('Pandan Fruit Cake', 'Pandan','Whipped Cream', 'Sliced Strawberries with Frosting', 'Sliced Kiwis with Frosting', ARRAY['Strawberries', 'Kiwis', 'Mandarin Oranges', 'Grapes'] , 45.00, 4);
 
-INSERT into `inventory` (`item_name`,`flavor`, `frosting`,`filling_one`, `filling_two`, `toppings`, `size`)
-VALUES ('Red Velvet Cake', 'Red Velvet','Cream Cheese', 'Just Frosting', 'None', ARRAY['No Toppings'], 4);
+INSERT into `inventory` (`item_name`,`flavor`, `frosting`,`filling_one`, `filling_two`, `toppings`, `size`,`image_url`)
+VALUES ('Red Velvet Cake', 'Red Velvet','Cream Cheese', 'Just Frosting', 'None', ARRAY['No Toppings'], 4, 'static/images/redVelvet.jpg');
 
-INSERT into `inventory` (`item_name`,`flavor`, `frosting`,`filling_one`, `filling_two`, `toppings`, `size`)
-VALUES ('Black Forest Cake', 'Chocolate','Whipped Cream','Just Frosting', 'Cherries with Frosting', ARRAY['Cherries', 'Chocolate Shavings'], 4);
+INSERT into `inventory` (`item_name`,`flavor`, `frosting`,`filling_one`, `filling_two`, `toppings`, `size`,`image_url`)
+VALUES ('Black Forest Cake', 'Chocolate','Whipped Cream','Just Frosting', 'Cherries with Frosting', ARRAY['Cherries', 'Chocolate Shavings'], 4, 'static/images/blackForest.jpg');
 
-INSERT into `inventory` (`item_name`,`flavor`,`frosting`,`filling_one`,`filling_two`,`toppings`,`size`)
-VALUES ('Carrot Cake', 'Carrot','Cream Cheese','Just Frosting','None', ARRAY['No Toppings' ], 4);
+INSERT into `inventory` (`item_name`,`flavor`,`frosting`,`filling_one`,`filling_two`,`toppings`,`size`,`image_url`)
+VALUES ('Carrot Cake', 'Carrot','Cream Cheese','Just Frosting','None', ARRAY['No Toppings' ], 4, 'static/images/carrotCake.jpeg');
 
-INSERT into `inventory` (`item_name`,`flavor`,`frosting`,`filling_one`,`filling_two`,`toppings`,`size`)
-VALUES ('Strawberry Shortcake', 'Shortcake','Whipped Cream','Just Frosting','Sliced Strawberries with Frosting', ARRAY['Strawberries'], 4);
+INSERT into `inventory` (`item_name`,`flavor`,`frosting`,`filling_one`,`filling_two`,`toppings`,`size`,`image_url`)
+VALUES ('Strawberry Shortcake', 'Shortcake','Whipped Cream','Just Frosting','Sliced Strawberries with Frosting', ARRAY['Strawberries'], 4, 'static/images/strawberryShortcake.jpg');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('1', 'aturing', '1', 10, '2022-12-21 7:30:30', 5.50);
